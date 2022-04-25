5d8a1754e315c59a576c2f73066fbd5554575801
Anant <95anantsingh@gmail.com>
2022-04-25 15:01:42 -0400

    update

--- zsession.sh
+++ zsession.sh
@@ -2,7 +2,8 @@
 # 1. CPU cores required
 # 2. RAM in GB
 # 3. GPU required
+# 4. Number of Hours
 
 echo
-echo "Getting Session with $1 Cores, $2 GB Ram, $3 GPUs..."
-srun -c$1 --mem="$2GB" --gres=gpu:rtx8000:$3 --pty /bin/bash
+echo "Getting Session with $1 Cores, $2 GB Ram, $3 GPUs for $4 Hours..."
+srun -c$1 --mem="$2GB" --gres=gpu:rtx8000:$3 -t$4:00:00 --pty /bin/bash
